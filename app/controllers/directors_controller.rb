7c87d1ade8a55af0b3a0fcb4d77bfa73f999e122
brylenelavelle Patrick <brylenelavelle@gmail.com>
2023-03-29 15:47:33 +0000

    director id

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,20 @@
+class DirectorsController < ApplicationController
+
+  def index
+    @list_of_directors = Director.all
+
+    render({ :template => "director_template/index.html.erb" })
+  end
+
+  def wisest
+    @oldest_director = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+    render({ :template => "director_template/eldest.html.erb" })
+  end
+
+  def director_details
+    # params looks like {"an_id" => "42"}
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({ :id => the_id }).at(0)
+    render({ :template => "director_template/show.html.erb" })
+  end
+end
